322ae5454b2b3e3eec0dfc590d8f1a036a3409dc
Dave Ames <david.john.ames@gmail.com>
2023-03-09 10:17:18 +0000

    Added this base class that the Exercise class inherits from with public definitions of the missing attributes for the Exercise class, that way the tests will fail rather than failing to run due to errors caused by the missing attributes.

---
+++ src/main/java/com/booleanuk/core/ExerciseBase.java
@@ -0,0 +1,14 @@
+package com.booleanuk.core;
+
+
+public class ExerciseBase {
+    public char lastLetter;
+    public float pi;
+    public double piD;
+    public String fullName;
+    public char tenthLetter;
+    public String lowerAlphabet;
+    public int alphabetLength;
+    public int remainder;
+
+}
